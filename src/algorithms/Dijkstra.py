51eb68a3ceb7c96967af4cc90e11d54bdca5a2da
Chu Minh <ciltron.com@gmail.com>
2023-06-01 16:27:33 +0700

    Done dijkstra

--- src/algorithms/Dijkstra.py
+++ src/algorithms/Dijkstra.py
@@ -4,14 +4,14 @@ Comsider maze as an 1D array of cells.
 
 from ..models.GoodCell import GoodCell as Cell 
 from ..models.GoodMaze import GoodMaze as Maze
-from Dijkstra import Dijkstra
+
 
 
 class My_Cell(Cell): 
     def __init__(self, 
                  x, 
                  y, 
-                 state, 
+                 status, 
                  visited, 
                  weight,
                  distance:float = float('inf')):
@@ -29,7 +29,7 @@ class My_Cell(Cell):
         - getDistance(): return distance
         """
 
-        super.__init__(self, x, y, state, visited, weight)
+        super().__init__(x, y, status, visited, weight)
         self.__distance = distance
 
     def setDistance(self, distance) -> None:
@@ -68,13 +68,13 @@ class Dijkstra:
         """
 
         #Convert to my maze and my cells
-        for x in maze.getWidth():
-            for y in maze.getHeight():
+        for x in range(maze.getWidth()):
+            for y in range(maze.getHeight()):
                 maze.getMaze()[x][y] = My_Cell(x, 
                                                y, 
                                                maze.getCell(x, y).getStatus(),
                                                maze.getCell(x, y).isVisited(),
-                                               maze.getCell(x, y).getWeight(),
+                                               maze.getCell(x, y).getWeight()
                                                )
 
         #Initialize distance of start to 0
@@ -82,7 +82,6 @@ class Dijkstra:
         start.setVisited(True) #Set start_cell to be visited
 
         #Initialize start_cells and path
-        best_path = []
         explored_path = []
         explored_path.append(start)
         start_cells = []
@@ -92,7 +91,7 @@ class Dijkstra:
         while True:
             #Get distances to neighbors
             for cell in start_cells:
-                for neighbor_cell in cell.getNeighbors(maze.getWidth(), maze.getHeight()):
+                for neighbor_cell in maze.getNeighbors(cell):
                     distance = cell.getDistance() + neighbor_cell.getWeight() #Get real distance to neighbor_cell
                     if neighbor_cell.isVisited() == False: #If neighbor_cell is not visited 
                         if neighbor_cell.getStatus() == 0: #If neighbor_cell is not wall
@@ -100,43 +99,49 @@ class Dijkstra:
                                 neighbor_cell.setDistance(distance)  
                                 neighbor_cell.setParent(cell) #Connect this cell to start cell
 
+            #If cell is end, break
+            for cell in start_cells:
+                if cell.getCoordinates() == end.getCoordinates():
+                    """
+                    Return path from start to end
+                    """
+                    path = []
+                    end = cell
+                    end.setVisited(True)
+                    while end.isVisited() == True:
+                        path.append(end)
+                        if end.getParent().isVisited() == True:
+                            end = end.getParent()
+                        if end == start:
+                            break
+                    path.append(start)
+                    path.reverse()
+                    return explored_path, path
+
             #Clear all start_cells
             start_cells.clear() 
             
             #Decide next cell to start
             min_dist = float('inf')
-            for cell in maze:
-                #Find min distance in unvisited cells
-                if cell.isVisited() == False and neighbor_cell.getStatus() == 0 \
-                    and min_dist > cell.getDistance():
-                    min_dist = cell.getDistance()
-
+            for x in range(maze.getWidth()):
+                for y in range(maze.getHeight()):
+                    #Find min distance in unvisited cells
+                    cell = maze.getCell(x, y)
+                    if cell.isVisited() == False and cell.getStatus() == 0 \
+                        and min_dist > cell.getDistance():
+                        min_dist = cell.getDistance()
+            
             #Find all cells with min distance
-            for cell in maze:
-                if cell.isVisited() == False and neighbor_cell.getStatus() == 0 \
-                    and cell.getDistance() == min_dist:
-                    start_cells.append(cell)
-                    cell.setVisited(True)
-                    explored_path.append(cell)
+            for x in range(maze.getWidth()):
+                for y in range(maze.getHeight()):
+                    cell = maze.getCell(x, y)
+                    if cell.isVisited() == False and cell.getStatus() == 0 \
+                        and cell.getDistance() == min_dist:
+                        start_cells.append(cell)#Append chosen cell to new start
+                        cell.setVisited(True)
+                        explored_path.append(cell)
                     
-
-            #If start_cell is end, break
-            for start_cell in start_cells:
-                if start_cell == end:
-                    """
-                    Return path from start to end
-                    """
-                    path = []
-                    print(end.isVisited())
-                    while end.isVisited() == True:
-                        print(end.getCoordinates())
-                        path.append(end.getCoordinates())
-                        end = end.getParent()
-                        if end == start:
-                            break
-                    path.append(start.getCoordinates())
-                    path.reverse()
-                    return (explored_path, path)
+            
 
                 
         
